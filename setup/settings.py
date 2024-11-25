6af6626552d321fda90fb93213cda2d04cc88eaf
lucalvex <lucalvex25@gmail.com>
2024-11-25 15:14:12 -0300

    Banco sqllite

--- setup/settings.py
+++ setup/settings.py
@@ -80,6 +80,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'items',
+#         'USER': 'admin',
+#         'PASSWORD': 'l@b!nf0',
+#         'HOST': 'localhost',  # Pode ser outro endereço se o banco de dados estiver em um servidor remoto
+#         'PORT': '3306', 
+#     }
+# }
+
 
 # Internationalization
 LANGUAGE_CODE = "pt-br"
