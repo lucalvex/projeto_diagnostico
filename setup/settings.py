e2da39be5301d89d4bcc6279d8f083323bc9c436
lucalvex <lucalvex25@gmail.com>
2024-11-25 15:08:50 -0300

    Configuração de banco realizado com banco mysql

--- setup/settings.py
+++ setup/settings.py
@@ -80,6 +80,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'diagnostico',
+        'USER': 'userAdmin',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',  # Pode ser outro endereço se o banco de dados estiver em um servidor remoto
+        'PORT': '3306', 
+    }
+}
+
 
 # Internationalization
 LANGUAGE_CODE = "pt-br"
